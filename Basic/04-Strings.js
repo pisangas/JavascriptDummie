67e086d44bbd064d6f3ca01f020e103bc1efe631
Eduardo Gil <egil@mobiletec.net>
2025-06-03 06:57:33 -0500

    Enhance string examples in 04-Strings.js and add new file 05-String-Practice.js

--- Basic/04-Strings.js
+++ Basic/04-Strings.js
@@ -19,3 +19,9 @@ console.log("Uppercase:", greeting.toUpperCase());
 console.log("Lowercase:", greeting.toLowerCase());
 //Finding the index of a substring
 console.log("Index of 'Eduardo':", greeting.indexOf("Eduardo")); 
+console.log(greeting.includes("Eduardo")); // true
+console.log(greeting.slice(0, 5)); // "Hello"
+console.log(greeting.replace("Hello", "Que hay")); // "Que hay, Eduardo! Welcome to the JavaScript world."
+
+let mail="notengo@notengo.com";
+console.log(`Interpolated string: ${greeting} su mail es: ${mail}`); // Using template literals for interpolation
