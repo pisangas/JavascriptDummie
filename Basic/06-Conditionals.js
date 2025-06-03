1ff8d3e1869fe17ad4d162fa4a66a87af3d645c7
Eduardo Gil <egil@mobiletec.net>
2025-06-03 10:44:13 -0500

    Update day variable in switch statement to reflect Thursday in 06-Conditionals.js

--- Basic/06-Conditionals.js
+++ Basic/06-Conditionals.js
@@ -11,7 +11,7 @@ const message = age >= 18 ? "You are an adult." : "You are a minor.";
 console.log(message);
 
 // Switch statement
-let day = 2;
+let day = 4;
 switch(day) {
     case 1:
         console.log("Monday");
