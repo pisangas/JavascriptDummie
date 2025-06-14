495a9b83198bebc4e7aab26234aed7a13168d637
Eduardo Gil <pisangas@gmail.com>
2025-06-13 20:24:05 -0500

    Add initial implementation of structures practice examples in 11-Structures-Practice.js

---
+++ Basic/11-Structures-Practice.js
@@ -0,0 +1,28 @@
+// 1. Crea un array que almacene cinco animales
+let MyAnimals = ["Perro", "Gato", "Elefante", "Tigre", "León"];
+console.log(MyAnimals); // Imprime el array actualizado
+
+// 2. Añade dos más. Uno al principio y otro al final
+MyAnimals.unshift("Loro"); // Añade "Loro" al principio
+MyAnimals.push("Conejo"); // Añade "Conejo" al final
+console.log(MyAnimals); // Imprime el array actualizado
+
+// 3. Elimina el que se encuentra en tercera posición
+MyAnimals.splice(2, 1); // Elimina el animal en la tercera posición (índice 2)
+console.log(MyAnimals); // Imprime el array actualizado
+
+// 4. Crea un set que almacene cinco libros
+let MyBooks = new Set(["1984", "El Principito", "Cien Años de Soledad", "Orgullo y Prejuicio", "Crimen y Castigo"]);
+console.log(MyBooks); // Imprime el set de libros
+
+// 5. Añade dos más. Uno de ellos repetido
+
+// 6. Elimina uno concreto a tu elección
+
+// 7. Crea un mapa que asocie el número del mes a su nombre
+
+// 8. Comprueba si el mes número 5 existe en el map e imprime su valor
+
+// 9. Añade al mapa una clave con un array que almacene los meses de verano
+
+// 10. Crea un Array, transfórmalo a un Set y almacénalo en un Map
