5c850d41de29a23d57dcc2bfaea6946533109a39
Eduardo Gil <pisangas@gmail.com>
2025-06-21 15:23:15 -0500

    Add forEach example to demonstrate array iteration in 14-Functios.js

--- Basic/14-Functios.js
+++ Basic/14-Functios.js
@@ -1,44 +1,59 @@
-// //Functions
-function getPerson(name, middleName, lastName) {
-    return `Hola, ${name} ${middleName} ${lastName} !`;
+// // //Functions
+// function getPerson(name, middleName, lastName) {
+//     return `Hola, ${name} ${middleName} ${lastName} !`;
+// }
+
+// console.log(getPerson("Eduardo", "Gil", "Rivas"));
+
+// //anonimas
+// const getPerson2 = function(name, middleName, lastName) {
+//     return `Hola, ${name} ${middleName} ${lastName} !`;
+// }
+
+// console.log(getPerson2("Eduardo", "Gil", "Rivas"));
+
+// //Arrow functions
+// const getPerson3 = (name, middleName, lastName) => {
+//     return `Hola, ${name} ${middleName} ${lastName} !`;
+// }
+
+// console.log(getPerson3("Eduardo", "Gil", "Rivas"));
+
+// //parameters
+// function sum(a, b) {
+//     return a + b;
+// }
+// console.log(sum(5, 10));
+
+// //default parameters
+// function sum2(a = 0, b = 0) {
+//     return a + b;
+// }
+// console.log(sum2(5));
+
+// //anidadas
+
+// function externalFunction() {
+//     function internalFunction() {
+//         return "Hola desde la funcion interna";
+//     }
+//     return internalFunction();
+// }
+
+// console.log(externalFunction());
+
+// //callback functions
+function greet(name, callback) {
+    console.log(`Hola, ${name}!`);
+    callback();
 }
 
-console.log(getPerson("Eduardo", "Gil", "Rivas"));
+greet("Eduardo", function() {
+    console.log("Esta es una función de callback que se ejecuta después del saludo.");
+});
 
-//anonimas
-const getPerson2 = function(name, middleName, lastName) {
-    return `Hola, ${name} ${middleName} ${lastName} !`;
-}
-
-console.log(getPerson2("Eduardo", "Gil", "Rivas"));
-
-//Arrow functions
-const getPerson3 = (name, middleName, lastName) => {
-    return `Hola, ${name} ${middleName} ${lastName} !`;
-}
-
-console.log(getPerson3("Eduardo", "Gil", "Rivas"));
-
-//parameters
-function sum(a, b) {
-    return a + b;
-}
-console.log(sum(5, 10));
-
-//default parameters
-function sum2(a = 0, b = 0) {
-    return a + b;
-}
-console.log(sum2(5));
-
-//anidadas
-
-function externalFunction() {
-    function internalFunction() {
-        return "Hola desde la funcion interna";
-    }
-    return internalFunction();
-}
-
-console.log(externalFunction());
-
+//ForEach
+const numbers = [1, 2, 3, 4, 5];
+numbers.forEach(function(number) {
+    console.log(`El número es: ${number}`);
+});
