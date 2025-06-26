8b788d0cf037420988d4ecb72fecf06d0d705190
Eduardo Gil <pisangas@gmail.com>
2025-06-25 19:36:15 -0500

    Add examples for object manipulation and create practice template in 16-Objects.js and 17-Objects-Practice.js

--- Basic/16-Objects.js
+++ Basic/16-Objects.js
@@ -29,3 +29,62 @@ console.log(persona); // { nombre: 'Juan', edad: '31', saludar: [Function: salud
 //agregar propiedades
 persona.apellido = "Pérez"; // Agrega una nueva propiedad apellido
 console.log(persona.apellido); // Pérez
+
+//anidar objetos
+let estudiante = {
+    nombre: "Ana",
+    edad: 22,
+    curso: {
+        nombreCurso: "JavaScript",
+        duracion: "3 meses",
+        profesor: {
+            nombre: "Carlos",
+            experiencia: "5 años"
+        }
+    },
+    saludar: function() {
+        console.log(`Hola, soy ${this.nombre} y estudio ${this.curso.nombreCurso}`);
+    }
+};
+console.log(estudiante);
+console.log(estudiante.curso.nombreCurso); // JavaScript
+console.log(estudiante.curso.profesor.nombre); // Carlos
+
+//igualdad de objects
+let personaSame = {
+    nombre: "Juan",
+    edad: 30,
+    profesion: "Ingeniero",
+    saludar: function() {
+        console.log(`Hola, mi nombre es ${this.nombre}`);
+    }
+};
+
+let personaSame2 = {
+    nombre: "Juan",
+    edad: 30,
+    profesion: "Ingeniero",
+    saludar: function() {
+        console.log(`Hola, mi nombre es ${this.nombre}`);
+    }
+};
+
+// Comparación de objetos por refrencia en memoria no por valor
+console.log(personaSame == personaSame2); // false, porque son dos objetos diferentes en memoria
+console.log(personaSame === personaSame2); // false, porque son dos objetos diferentes en memoria
+
+//Iterar
+for (let key in personaSame2) {
+    console.log(`${key}: ${persona[key]}`);
+}
+
+//Funciones como bjects
+function crearPersona(nombre, edad) {
+    return {
+        nombre: nombre,
+        edad: edad,
+        saludar: function() {
+            console.log(`Hola, mi nombre es ${this.nombre} y tengo ${this.edad} años.`);
+        }
+    };
+}
