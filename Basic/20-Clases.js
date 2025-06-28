ed6d4d827f815cd7f8a5075854891db1bb60c00f
Eduardo Gil <pisangas@gmail.com>
2025-06-28 16:30:50 -0500

    Implement PrivatePersona class with encapsulation and inheritance; add MathOperations class with static methods

--- Basic/20-Clases.js
+++ Basic/20-Clases.js
@@ -35,3 +35,78 @@ console.log(estudiante1.apellido);
 estudiante1.apellido = "Pérez";
 console.log(estudiante1.apellido); // Muestra "Pérez"
 
+//encapsular
+class PrivatePersona {
+    #saldo; // Propiedad privada
+    constructor(nombre, edad, profesion, saldo) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.profesion = profesion;
+        this.#saldo = saldo;
+    }
+
+    modificarSaldo(){
+        this.#saldo += 1000; // Modifica el saldo
+    }
+    get saldo() {
+        return console.log(`El saldo de ${this.nombre} es ${this.#saldo}`);
+    }
+
+    set saldo(nuevoSaldo) {
+        this.#saldo = nuevoSaldo; // Modifica el saldo
+    }
+
+}
+
+
+let privatePersona = new PrivatePersona("Ana", 25, "Diseñadora", 5000);
+
+privatePersona.modificarSaldo(); // Modifica el saldo
+privatePersona.saldo; // Muestra "El saldo de Ana es 6000"
+privatePersona.saldo = 7000; // Modifica el saldo
+privatePersona.saldo; // Muestra "El saldo de Ana es 7000"
+
+//Herencia
+
+class Animal{
+    constructor(nombre){
+        this.nombre = nombre;
+    }
+    hacerSonido() {
+        console.log(`${this.nombre} hace un sonido.`);
+    }
+}
+
+class Perro extends Animal {
+    constructor(nombre, especie) {
+        super(nombre); // Llama al constructor de la clase padre
+        this.especie = especie;
+    }
+    corre() {
+        console.log(`${this.nombre} corre.`);
+    }
+
+    hacerSonido() {
+        console.log(`${this.nombre} ladra.`);
+    }
+}
+
+let myDog = new Perro("Rex", "Labrador");
+myDog.hacerSonido(); 
+myDog.corre(); // Muestra "Rex corre."
+
+
+//metodos estaticos
+class MathOperations {
+    static suma(a, b) {
+        return a + b;
+    }
+
+    static resta(a, b) {
+        return a - b;
+    }
+}
+
+console.log(MathOperations.suma(5, 3)); // Muestra 8
+console.log(MathOperations.resta(5, 3)); // Muestra 2
+
