586b2ab47c4bcc29d0d67f49b96443803b537e15
Eduardo Gil <pisangas@gmail.com>
2025-06-28 15:59:35 -0500

    Add default values to Estudiante class constructor and demonstrate usage

--- Basic/20-Clases.js
+++ Basic/20-Clases.js
@@ -17,3 +17,21 @@ let juan = new Persona("Juan", 30, "Ingeniero");
 juan.saludar(); // Muestra "Hola, mi nombre es Juan"
 console.log(juan.edad); // Muestra 30
 console.log(juan.profesion); // Muestra "Ingeniero"
+
+//valores por defecto
+class Estudiante {
+    constructor(nombre = "N", apellido = "N", edad = 0, curso = "Desconocido") {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+        this.curso = curso;
+    }
+}
+
+let estudiante1 = new Estudiante();
+console.log(estudiante1.nombre); // Muestra "N"
+console.log(estudiante1.apellido); 
+
+estudiante1.apellido = "Pérez";
+console.log(estudiante1.apellido); // Muestra "Pérez"
+
