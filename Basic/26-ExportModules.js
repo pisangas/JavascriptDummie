47b68f303628d45f0038bb3fd25ad6b22af7aba2
Eduardo Gil <pisangas@gmail.com>
2025-08-08 19:08:24 -0500

    Import - Export Files

---
+++ Basic/26-ExportModules.js
@@ -0,0 +1,6 @@
+//Exportacion de modulos
+
+export function greet(name) {
+    return `Hello, ${name}! desde 26-ExportModules.js`;
+
+}
---
+++ Basic/27-ImportModules.js
@@ -0,0 +1,7 @@
+//Importacion modulos
+
+// funciones
+import { greet } from './26-ExportModules.js';
+
+greet('Egil'); // Invokes the greet function from the imported module
+console.log(greet('Egil')); // Outputs: Hello, Egil!
