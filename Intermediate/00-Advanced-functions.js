2dbbe1ce05430cb8e48418df39260dd8f2540468
Eduardo Gil <pisangas@gmail.com>
2025-08-17 12:54:36 -0500

    parametros REST

--- Intermediate/00-Advanced-functions.js
+++ Intermediate/00-Advanced-functions.js
@@ -58,3 +58,14 @@ car.move();
 (() => {
     console.log("IIFE Classic Arrow");
 })();
+
+//REST (...) parameters - es posible combinarlos y los convierte en array
+function sum(...numbers) { 
+    let result = 0;
+    for (let number of numbers) {
+        result += number;
+    }   
+    return result;
+}
+
+console.log(sum(1, 2, 3, 4, 5)); //15
