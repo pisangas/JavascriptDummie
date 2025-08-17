7a24ff3dcdb04c2c113bcf46ebe7cffc9e468889
Eduardo Gil <pisangas@gmail.com>
2025-08-16 22:05:34 -0500

    By Default Export

--- Basic/26-ExportModules.js
+++ Basic/26-ExportModules.js
@@ -13,3 +13,20 @@ export const  propertySource = ' property desde 26-ExportModules.js'; // Example
 export default function defaultGreet(name) {
     return `Default greeting for ${name}`;
 }
+
+//Clases Export
+
+export class Circle {
+    constructor(radius) {
+        this.radius = radius;
+    }
+
+    area() {
+        return Math.PI * Math.pow(this.radius, 2);
+    }
+
+perimeter() {
+        return 2 * Math.PI * this.radius;
+    }
+
+}
--- Basic/27-ImportModules.js
+++ Basic/27-ImportModules.js
@@ -1,7 +1,7 @@
 //Importacion modulos
 
 // funciones
-import defaultGreet, { greet, propertySource, fileSource } from './26-ExportModules.js';
+import defaultGreet, { greet, propertySource, fileSource, Circle } from './26-ExportModules.js';
 
 greet('Egil'); // Invokes the greet function from the imported module
 console.log(greet('Egil') + propertySource); 
@@ -9,3 +9,7 @@ console.log(greet('Egil') + fileSource);
 
 // Display the imported property values
 console.log(defaultGreet('Egil')); // Invokes the default exported function
+
+let circle = new Circle(5); // Create an instance of the Circle class
+console.log(`Area of the circle: ${circle.area().toFixed(4)}`); // Display the area of the circle
+console.log(`Perimeter of the circle: ${circle.perimeter().toFixed(4)}`); // Display the perimeter of the circle
