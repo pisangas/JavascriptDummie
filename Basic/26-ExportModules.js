6df604a2a59e30bbfae27c74e0a33491e3c8b76e
Eduardo Gil <pisangas@gmail.com>
2025-08-16 21:59:31 -0500

    Import Export xamples

--- Basic/26-ExportModules.js
+++ Basic/26-ExportModules.js
@@ -1,6 +1,15 @@
 //Exportacion de modulos
 
 export function greet(name) {
-    return `Hello, ${name}! desde 26-ExportModules.js`;
+    return `Hello, ${name}!`;
+}
 
+// Propiedades de exportacion
+export const  fileSource = ' file desde 26-ExportModules.js'; // Example of a named export
+export const  propertySource = ' property desde 26-ExportModules.js'; // Example of a named export
+
+//Export by default
+//solo sepuede exportar una funcion por defecto
+export default function defaultGreet(name) {
+    return `Default greeting for ${name}`;
 }
--- Basic/27-ImportModules.js
+++ Basic/27-ImportModules.js
@@ -1,7 +1,11 @@
 //Importacion modulos
 
 // funciones
-import { greet } from './26-ExportModules.js';
+import defaultGreet, { greet, propertySource, fileSource } from './26-ExportModules.js';
 
 greet('Egil'); // Invokes the greet function from the imported module
-console.log(greet('Egil')); // Outputs: Hello, Egil!
+console.log(greet('Egil') + propertySource); 
+console.log(greet('Egil') + fileSource); 
+
+// Display the imported property values
+console.log(defaultGreet('Egil')); // Invokes the default exported function
